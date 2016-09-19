0099200cdd1e9fcd0e2d1a276d9005ebd6a9eb29
Ahmad Ahmadi & Ryan Andhaka <pair+ahmadahmadi14.ryanandhaka@hacktiv8.com>
2016-09-19 17:05:50 +0700

    Ahmad Ryan

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', text:" " });
+});
+router.post('/', function(req, res, next) {
+  var string = req.body.teksname;
+  string = string.toLowerCase();
+  teksname_array =string.split(' ');
+  if(teksname_array.length<=1){
+  teksname ="masukkan minimal dua kata"
+  }
+  res.render('index', { title: 'Express', text:string });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('Your respond');
 // });
-
+// release 1
 // router.get('/', function(req, res, next) {
 //   res.send(req.query.say);
 // });
 
 router.get('/', function(req, res, next) {
-  res.send('req.query.param1');
+  res.render('index', { title: 'Express', text:" " });
 });
+router.post('/', function(req, res, next) {
+  var string = req.body.teksname;
+  string = string.toLowerCase();
+  teksname_array =string.split(' ');
+  if(teksname_array.length <= 1){
+  teksname ="masukkan minimal dua kata"
+  }
+  res.render('index', { title: 'Express', text:string });
+});
+ // router.get('/', function(req, res, next) {
+ //  res.send('req.query.param1&&req.query.param1');
+ // });
+
 
 
 module.exports = router;
